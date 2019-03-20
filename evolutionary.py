d0fa14eb829bc204612225a7175f0dbc6d97586d
udesc <marcelonunespaolillo@gmail.com>
2019-03-20 16:48:56 -0300

    .

--- evolutionary.py
+++ evolutionary.py
@@ -10,7 +10,7 @@ import numpy as np
 
 class EvolutionaryAlgorithm():
 
-    def __init__(self,COD=None,D=None,POP=None,bounds=None,seed=None):
+    def __init__(self,COD=None,D=None,POP=None,low_bound=None,high_bound=None,seed=None):
         """
         COD param: str, ('bin','int', 'int-perm', 'real') codificações possiveis
         D: int, tamanho do cromossomo (número de variáveis)
@@ -18,13 +18,14 @@ class EvolutionaryAlgorithm():
         bounds param: tuple, (lim_inferior,lim_superior)
         seed param: int, seed para geração de números aleatórios
         """
-        self.params_are_valid(COD=COD,D=D,POP=POP,bounds=bounds)
+        self.params_are_valid(COD=COD,D=D,POP=POP,low_bound=low_bound,high_bound=high_bound)
 
         self.list_pop = []
         self.COD = COD
         self.D = D
         self.POP = POP
-        self.bounds = bounds
+        self.low_bound = low_bound
+        self.high_bound = high_bound
         self.seed = seed
 
         print ("Gerando População inicial...")
@@ -36,7 +37,8 @@ class EvolutionaryAlgorithm():
         Gera a população com base nos parametros de inicialização
         """
         for _ in range(self.POP):
-            self.list_pop.append(Individual(COD=self.COD,D=self.D,bounds=self.bounds,seed=self.seed))
+            self.list_pop.append(Individual(COD=self.COD,D=self.D,low_bound=self.low_bound,
+                                                high_bound=self.high_bound,seed=self.seed))
 
     def info(self):
         print ("[Info]")
@@ -49,10 +51,10 @@ class EvolutionaryAlgorithm():
     def print_pop(self):
         print ("[Population = "+str(self.POP)+"]")
         for i in range(0,len(self.list_pop)):
-            print (" - " + str(i+1) + " ->",self.list_pop[i].chromossome)
+            print (" - " + str(i) + " ->",self.list_pop[i].chromossome)
         return
 
-    def params_are_valid(self,COD, D, POP, bounds):
+    def params_are_valid(self,COD, D, POP, low_bound,high_bound):
         if POP <= 0:
             raise ValueError(POP + ": Tamanho de população inválido")
 
@@ -60,10 +62,10 @@ class EvolutionaryAlgorithm():
             raise ValueError(COD + ": Codificação inválida ou não suportada")
 
         if COD in ['int','real']:
-            if (bounds is None) or (bounds[0] >= bounds[1]) or (not isinstance(bounds,tuple)) or (len(bounds)>2):
+            if (bounds is None) or (low_bound >= high_bound) or (not isinstance(bounds,tuple)) or (len(bounds)>2):
                 raise ValueError(COD + ": Combinação de parâmetros inválida")
         elif COD == 'int-perm':
-            if (bounds is None) or len(list(range(bounds[0],bounds[1])))!=D:
+            if (bounds is None) or len(list(range(low_bound,high_bound)))!=D:
                 raise ValueError(COD + ": Combinação de parâmetros inválida")
         elif COD == 'bin':
             if bounds is not None:
@@ -106,28 +108,24 @@ class Individual():
         print (self.chromossome)
         return
 
+def read_input(input_path):
+    f = open(input_path, "r")
+    COD = f.readline().split("=")[1]
+    D = int(f.readline().split("=")[1])
+    POP = int(f.readline().split("=")[1])
+    low_bound = int(f.readline().split("=")[1])
+    high_bound = int(f.readline().split("=")[1])
 
+    return (COD,D,POP,low_bound,high_bound,seed)
 
 if __name__ == "__main__":
 
-
-
+    COD,D,POP,low_bound,high_bound,seed = read_input('path')
     start = time.time()
-    ae = EvolutionaryAlgorithm('bin',D=10,POP=10)
+    ae = EvolutionaryAlgorithm(COD,D,POP,low_bound,high_bound,seed)
     ae.info()
     ae.print_pop()
-
-    ae = EvolutionaryAlgorithm('int',D=10,POP=10,bounds=(-5,5))
-    ae.info()
-    ae.print_pop()
-
-    ae = EvolutionaryAlgorithm('int-perm',D=10,POP=10,bounds=(0,10))
-    ae.info()
-    ae.print_pop()
-
-    ae = EvolutionaryAlgorithm('real',D=10,POP=10,bounds=(-10,10))
-    ae.info()
-    ae.print_pop()
+    print (str(ae.list_pop[0].chromossome))
 
     end = time.time() - start
 
